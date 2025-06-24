e28c6352500a357ddef77b0203e85c5bdc23a7bb
blueprint11 <goyalakshay02gmail.com>
2025-06-24 16:30:21 +0000

    created a player in centre of screen ,though a circle visualised as triangle

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import pygame
 from constants import *
+from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     black=(0,0,0)
     obj=pygame.time.Clock()
+    player=Player(x = SCREEN_WIDTH / 2,y = SCREEN_HEIGHT / 2)
     dt=0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(black)
+        player.draw(screen)
         pygame.display.flip()
         obj.tick(60)
         dt=obj.tick(60)/1000
+    
 
 if __name__ == "__main__":
     main()
