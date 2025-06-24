7bb038e6025717f8f979e287479d6eda06087fe1
blueprint11 <goyalakshay02gmail.com>
2025-06-24 19:51:15 +0000

    player can move right and left now

--- player.py
+++ player.py
@@ -6,7 +6,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation=0
-    
+    #to make circle seem like a rectangle
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -14,5 +14,18 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+    #to draw on screen
     def draw(self, screen):
         pygame.draw.polygon(screen,"white",self.triangle(),width=2)
+    #for rotating
+    def rotate(self,dt):
+        self.rotation+=PLAYER_TURN_SPEED*dt
+    #binding pygame.key to keyboard to rotate
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+
+        if keys[pygame.K_d]:
+            self.rotate(dt)
