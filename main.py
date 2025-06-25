1b76f446e40e5ced199adbb9e2fc86ed2c323c45
blueprint11 <goyalakshay02gmail.com>
2025-06-25 08:08:02 +0000

    asteroid_field created ,asteroids spawned

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     black=(0,0,0)
     obj=pygame.time.Clock()
 
+    asteroids=pygame.sprite.Group()
     updatable=pygame.sprite.Group()
     drawable=pygame.sprite.Group()
     Player.containers = (updatable,drawable)
-    
+    Asteroid.containers=(asteroids,updatable,drawable)
+    AsteroidField.containers=(updatable)
+
+    aster_f=AsteroidField()
+
     player=Player(x = SCREEN_WIDTH / 2,y = SCREEN_HEIGHT / 2)
     dt=0
 
